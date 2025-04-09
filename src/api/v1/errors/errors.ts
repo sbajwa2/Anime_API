0dec12677ac67b958b158de542294ec7c6411b09
Smilepreet Kaur <sbajwa2@rrc.academic.ca>
2025-04-09 12:46:47 -0500

    improve error handling with custom error classes

--- src/api/v1/errors/errors.ts
+++ src/api/v1/errors/errors.ts
@@ -8,7 +8,7 @@ export class AppError extends Error {
     constructor(message: string, statusCode: number) {
       super(message);
       this.statusCode = statusCode;
-      this.isOperational = true; // Set to true for known errors (like validation)
+      this.isOperational = true; 
       Error.captureStackTrace(this, this.constructor);
     }
   }
